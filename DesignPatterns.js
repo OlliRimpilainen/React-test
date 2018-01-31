d4f865c5ca16555f32cafa509991e5e29291a284
Olli RimpiläinenpOlliasdasdzx <olli.rimpilazxczxinen@roimaint.com>
2018-01-31 16:34:36 +0200

    asd

---
+++ DesignPatterns.js
@@ -0,0 +1,20 @@
+var task = {};
+task.title = 'My Task';
+task.description = "My Desctiption";
+
+task.toString = function (){
+    return this.title + ' ' + this.description;
+}
+
+console.log(task.toString());
+
+
+
+
+
+
+
+
+
+
+
