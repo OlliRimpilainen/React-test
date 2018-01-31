629ebdee107c3a5fc50f3f31e5ef3e1f722b82d7
Olli RimpiläinenpOlliasdasdzx <olli.rimpilazxczxinen@roimaint.com>
2018-01-31 16:55:50 +0200

    as

---
+++ task.js
@@ -0,0 +1,25 @@
+var Task = function(name){
+    this.name = name;
+    this.completed = false;
+
+    this.complete = function(){
+        console.log("compliting task: " + Task.name);
+        
+        this.completed = true;
+    }
+
+    this.save = function(){
+        console.log('saving Task: ' + this.name);
+    }
+}
+
+var task1 = new Task('create a demo for constructor');
+var task2 = new Task('create a demo for modules');
+var task3 = new Task('create a demo for singletons');
+var task4 = new Task('create a demo for prototypes');
+
+task1.complete();
+task2.save();
+
+
+
